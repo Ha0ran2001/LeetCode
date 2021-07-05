2db43e581f4ff2851e7d5db497715c60f107e02d
Rookoe-haoran <bababylon88@gmail.com>
2021-07-05 23:46:58 +0800

    add leetcode-203.移除链表元素

---
+++ 链表/leetcode#203.移除链表元素.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  let dummyHead = new ListNode(0); // 哑结点
+  dummyHead.next = head;
+  let p = dummyHead;
+  while (p.next) {
+    if (p.next.val === val) {
+      p.next = p.next.next;
+    } else {
+      p = p.next;
+    }
+  }
+  return dummyHead.next;
+};
