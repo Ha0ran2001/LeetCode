248db40198ecf386d4baa193a4bae6e4d2426ac0
Rookoe-haoran <bababylon88@gmail.com>
2021-07-10 15:27:05 +0800

    add 剑指offer-18删除链表的节点

---
+++ 链表/剑指offer#18.删除链表的节点.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var deleteNode = function (head, val) {
+  let dummyHead = new ListNode(0);
+  dummyHead.next = head;
+  let p = dummyHead;
+  while (p.next) {
+    if (p.next.val === val) {
+      p.next = p.next.next;
+    } else {
+      p = p.next;
+    }
+  }
+  return dummyHead.next;
+};
