c82d1a811f3190d924f5bb5a2d9f374f426adb4d
Rookoe-haoran <bababylon88@gmail.com>
2021-08-23 20:49:58 +0800

    feat: fuking leetcode-226

---
+++ Fucking BT/leetcode#226.翻转二叉树.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  // base case
+  if (root === null) {
+    return null;
+  }
+  // 前序遍历: 根 -> 左 -> 右
+  //交换
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  invertTree(root.left);
+  invertTree(root.right);
+  return root;
+};
