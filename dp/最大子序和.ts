2c27cce1b649e3271042af74d26c3a08eaad44be
Rookoe-haoran <bababylon88@gmail.com>
2021-10-22 19:42:43 +0800

    feat: 最大子序和

---
+++ dp/最大子序和.ts
@@ -0,0 +1,8 @@
+function maxSubArray(nums: number[]): number {
+  let pre = 0, maxAns = nums[0];
+  nums.forEach(x => {
+    pre = Math.max(pre + x, x);
+    maxAns = Math.max(pre, maxAns);
+  });
+  return maxAns;
+};
