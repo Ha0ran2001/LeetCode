07c8293c420aef386d71c838c8d642fb761b9606
Rookoe-haoran <bababylon88@gmail.com>
2022-01-04 13:09:50 +0800

    feat: 在排序数组中查找数字1

---
+++ 查找/剑指Offer53.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let count = 0;
+  nums.forEach((item) => {
+    if (item === target) {
+      count++;
+    }
+  })
+  return count;
+};
