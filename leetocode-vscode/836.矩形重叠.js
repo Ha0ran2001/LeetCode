a63826b70e6758171074f9000e4a52512526ba46
Rookoe-haoran <bababylon88@gmail.com>
2022-08-17 18:10:40 +0800

    坐标题

---
+++ leetocode-vscode/836.矩形重叠.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=836 lang=javascript
+ *
+ * [836] 矩形重叠
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} rec1
+ * @param {number[]} rec2
+ * @return {boolean}
+ */
+var isRectangleOverlap = function (rec1, rec2) {
+	return (
+		Math.min(rec1[2], rec2[2]) > Math.max(rec1[0], rec2[0]) &&
+		Math.min(rec1[3], rec2[3]) > Math.max(rec1[1], rec2[1])
+	)
+}
+// @lc code=end
