938158deb749355c8279688c4c1d8369f8c14b40
Rookoe-haoran <bababylon88@gmail.com>
2022-08-19 14:58:37 +0800

    反转链表

--- leetocode-vscode/92.反转链表-ii.js
+++ leetocode-vscode/92.反转链表-ii.js
@@ -18,36 +18,52 @@
  * @param {number} right
  * @return {ListNode}
  */
-var reverseBetween = function (head, left, right) {
-	if (head === null || right < left) {
-		return head
-	}
-	let dummy = new ListNode(-1, head)
-	let preLeftNode = dummy
-	for (let i = 0; i < left - 1; i++) {
-		preLeftNode = preLeftNode.next
-	}
-	let rightNode = preLeftNode
-	for (let i = 0; i < right - left + 1; i++) {
-		rightNode = rightNode.next
-	}
-	let begin = preLeftNode.next
-	let end = rightNode.next
-	let h = reverse(begin, end)
-	preLeftNode.next = h
-	begin.next = end
-	return dummy.next
-}
 
-const reverse = (begin, end) => {
-	let dummy = new ListNode(-1)
-	let p = begin
-	while (p !== end) {
-		const back = p.next
-		p.next = dummy.next
-		dummy.next = p
-		p = back
+var reverseBetween = function (head, left, right) {
+	let dummy = new ListNode(-1, head)
+	let pre = dummy
+	for (let i = 0; i < left - 1; i++) {
+		pre = pre.next
+	}
+	let cur = pre.next
+	for (let i = left; i < right; i++) {
+		let next = cur.next
+		cur.next = next.next
+		next.next = pre.next
+		pre.next = next
 	}
 	return dummy.next
 }
+// var reverseBetween = function (head, left, right) {
+// 	if (head === null || right < left) {
+// 		return head
+// 	}
+// 	let dummy = new ListNode(-1, head)
+// 	let preLeftNode = dummy
+// 	for (let i = 0; i < left - 1; i++) {
+// 		preLeftNode = preLeftNode.next
+// 	}
+// 	let rightNode = preLeftNode
+// 	for (let i = 0; i < right - left + 1; i++) {
+// 		rightNode = rightNode.next
+// 	}
+// 	let begin = preLeftNode.next
+// 	let end = rightNode.next
+// 	let h = reverse(begin, end)
+// 	preLeftNode.next = h
+// 	begin.next = end
+// 	return dummy.next
+// }
+
+// const reverse = (begin, end) => {
+// 	let dummy = new ListNode(-1)
+// 	let p = begin
+// 	while (p !== end) {
+// 		const back = p.next
+// 		p.next = dummy.next
+// 		dummy.next = p
+// 		p = back
+// 	}
+// 	return dummy.next
+// }
 // @lc code=end
